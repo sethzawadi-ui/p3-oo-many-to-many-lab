1efe1cd3ff0e99fd2b1068d254fc85b0992a669b
sethzawadi-ui <seth.zawadi@student.moringaschool.com>
2025-12-05 18:39:41 +0300

    many to many labs

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,62 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise TypeError("author must be an Author instance")
+        if not isinstance(book, Book):
+            raise TypeError("book must be a Book instance")
+        if not isinstance(date, str):
+            raise TypeError("date must be a string")
+        if not isinstance(royalties, int):
+            raise TypeError("royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date=None):
+        if date is None:
+            # If no date is provided, return all contracts sorted by date
+            return sorted(cls.all, key=lambda c: c.date)
+        # If date is provided, filter by that date
+        return sorted([c for c in cls.all if c.date == date], key=lambda c: c.date)
+
